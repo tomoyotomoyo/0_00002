aa93901c3efa93f368206ebbd73962f8c95c32b3
想吃芝士条 <wwwwxzyz@icloud.com>
2026-08-23 18:54:01 +0800

    Add files via upload

---
+++ app/AppDelegate.m
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.m
+//
+
+#import "AppDelegate.h"
+#import "RootViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[RootViewController alloc] init];
+    [self.window makeKeyAndVisible];
+
+    // Audio background keepalive
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback
+                                       withOptions:AVAudioSessionCategoryOptionMixWithOthers
+                                             error:nil];
+    [[AVAudioSession sharedInstance] setActive:YES error:nil];
+
+    return YES;
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [[UIApplication sharedApplication] beginBackgroundTaskWithName:@"KFLogKeepalive" expirationHandler:^{}];
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    extern void KFShutdown(void);
+    KFShutdown();
+}
+
+@end
